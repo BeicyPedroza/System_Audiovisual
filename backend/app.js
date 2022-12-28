9d9346971bb2d967da80c74e6adae6d71a9de832
Beicy Pedroza <beyjara77@gmail.com>
2022-12-27 21:46:27 -0500

    Configuracion del backend

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,30 @@
-import express from "express"
+import express from "express";
 import mongoose from "mongoose";
-import dotenv  from "dotenv";
+import dotenv from "dotenv";
 import userRouter from "./router/userRouter.js";
 import testRouter from "./router/testRouter.js";
-import reservasRouter from "./router/reservasRouter.js";    
+import reservasRouter from "./router/reservasRouter.js";
 import clientRouter from "./router/clientRouter.js";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.listen(port, ()=>{
-    console.log("El servidor se esta ejecutando correctamente.");
-})
+app.listen(port, () => {
+  console.log("El servidor se esta ejecutando correctamente.");
+});
 
 //mongodb connection
-mongoose.set("strictQuery",true)
-mongoose.connect(process.env.MONGODB_URI).then(()=> console.log("Conectado a la DB MongoDB Atlas"))
-.catch((error)=> console.error(error));
-
-
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Conectado a la DB MongoDB Atlas"))
+  .catch((error) => console.error(error));
 
 //middleware
-app.use(express.json())
-app.use("/user", userRouter)
-app.use("/client", clientRouter)
-app.use("/reservas", reservasRouter)
-app.use("/test", testRouter)
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/client", clientRouter);
+app.use("/reservas", reservasRouter);
+app.use("/test", testRouter);
